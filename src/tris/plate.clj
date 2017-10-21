880400e9b72af784fc650d84ebf9e42f5ff545ea
Arkadiy Drovosekov <arkdro@gmail.com>
2017-10-21 23:50:39 +0200

    build one point

--- src/tris/plate.clj
+++ src/tris/plate.clj
@@ -1,9 +1,15 @@
 (ns tris.plate)
 
+(defn build_one_point
+  "Build one point"
+  [point_range]
+  3)
+
 (defn build_plate
   "Build a plate filled with grains"
   [{width :width
     height :height
     point_range :point_range}]
+  (build_one_point point_range)
   [width height point_range])
 
